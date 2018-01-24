3501d524fc777ad90d3a274b3f0b20df55b7b1f1
Finance app <dylangodfrey12@gmail.com>
2018-01-24 16:16:32 +0000

    might work in heroku now2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'emails/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'home#index'
-  
+   get 'emails/new'
   get 'projects', to:'pages#projects'
   get 'about', to:'pages#about'
   get 'contact', to:'pages#contact'
   get 'services', to:'pages#services'
-  get 'thankyou', to:'pages#thankyou'
-  resources :emails
+  
+   get 'thankyou', to:'pages#thankyou'
+ resources :emails
 end
