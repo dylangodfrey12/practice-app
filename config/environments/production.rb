64fcc4c53991289ca61d6cfb17eb353946c9afe2
Finance app <dylangodfrey12@gmail.com>
2018-02-13 21:53:37 +0000

    updated portfolio2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,10 @@ config.assets.compile = true
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "practice-app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
