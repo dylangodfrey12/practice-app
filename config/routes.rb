c29c6c80d212aa99ed76408f851b2b67eaa11508
Finance app <dylangodfrey12@gmail.com>
2018-02-13 21:21:12 +0000

    updated portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'home#index'
+  root 'pages#about'
   
    get 'emails/new', to:'emails#new'
    get 'emails', to:'emails#new'
    
   get 'projects', to:'pages#projects'
-  get 'about', to:'pages#about'
+ 
   get 'contact', to:'pages#contact'
 
    get 'thankyou', to:'pages#thankyou'
